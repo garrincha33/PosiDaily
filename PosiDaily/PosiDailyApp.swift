1a75bc9992ac1e4e01cfbca4ae1022053b1a1c7a
Richy <garrincha33@users.noreply.github.com>
2023-04-05 14:47:07 +0100

    Initial Commit

---
+++ PosiDaily/PosiDailyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PosiDailyApp.swift
+//  PosiDaily
+//
+//  Created by Richard Price on 05/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct PosiDailyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
