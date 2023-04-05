febdc98a8d8014f9fa7220b58b8079a8df01f1a0
Richy <garrincha33@users.noreply.github.com>
2023-04-05 14:50:57 +0100

    inital commit

--- PosiDaily/Main/Main.swift
+++ PosiDaily/Main/Main.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  Main.swift
 //  PosiDaily
 //
 //  Created by Richard Price on 05/04/2023.
@@ -7,20 +7,20 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct Main: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("Hello, Main!")
         }
         .padding()
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
+struct Main_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Main()
     }
 }
