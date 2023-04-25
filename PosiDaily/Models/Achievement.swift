d4fe8a5d8487b7f737bcf7d9ef34c6d211444121
Richy <garrincha33@users.noreply.github.com>
2023-04-25 17:56:40 +0100

    added achievement model and test to init the achievement

---
+++ PosiDaily/Models/Achievement.swift
@@ -0,0 +1,15 @@
+//
+//  Achievement.swift
+//  PosiDaily
+//
+//  Created by Richard Price on 25/04/2023.
+//
+
+import Foundation
+
+struct Achievement {
+    let id: UUID
+    let title: String
+    let description: String
+    let isUnlocked: Bool
+}
